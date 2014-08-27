fffdbe5707f55ee5766f3ec16c1df9c99ffd8fbb
nitinha <nitin.harlalka@gmail.com>
2014-08-27 10:06:32 +0530

    Update next_larger_num.cpp

--- next_larger_num.cpp
+++ next_larger_num.cpp
@@ -1,3 +1,8 @@
+//Make larger number <http://www.dsalgo.com/2013/02/NextLargerNumber.php.html>
+//Programmer : Nitin Harlalka
+//Input : An Integer Value
+//Output : The next possible larger integer value
+
 #include <iostream>
 #include <iterator>
 #include <vector>
@@ -8,17 +13,21 @@ int main(){
     int num;
     cin >> num;
 
+//Storing the input number as a array of single digit numbers
     vector <int> num_arr_rev = vector<int>();
     int temp_num = num;
     while(temp_num > 0){
         num_arr_rev.push_back(temp_num % 10);
         temp_num /= 10;
     }
-
+    
+//Since the above vector contains the array of numbers stored in reverse format, reversing it again 
     vector <int> num_arr = vector<int>();
     for (vector<int>::reverse_iterator it = num_arr_rev.rbegin() ; it != num_arr_rev.rend(); ++it)
         num_arr.push_back(*it);
 
+//Traversing the array from right
+//Finding the first decreasing sequence
     int larger_num_index = num_arr.size() - 1;
     int smaller_num_index = num_arr.size() - 2;
 
@@ -32,17 +41,19 @@ int main(){
             smaller_num_index = i - 1;
         }
 
-
+//Breaking out of the loop when the first decreasing sequence is found
         if(larger_num_index < smaller_num_index)
             break;
     }
 
+//Swapping the larger and smaller numbers
     if(larger_num_index < smaller_num_index){
         int temp = num_arr[smaller_num_index];
         num_arr[smaller_num_index] = num_arr[larger_num_index];
         num_arr[larger_num_index] = temp;
     }
 
+//Converting the array back to a single integer
     int larger_num = 0;
     int num_len = 0;
     while(num_len < num_arr.size()){
